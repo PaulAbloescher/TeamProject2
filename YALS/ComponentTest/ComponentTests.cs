41a4d6e9fe57986a8b6fb558b26893e5b2a389d2
Lett1 <lett@gmx.at>
2019-01-15 16:07:56 +0100

    add basic testing

---
+++ YALS/ComponentTest/ComponentTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace ComponentTest
+{
+    [TestFixture]
+    public class ComponentTests
+    {
+        [Test]
+        public void Example()
+        {
+            Assert.Pass();
+        }
+    }
+}
