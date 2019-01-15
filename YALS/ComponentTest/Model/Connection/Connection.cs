035e244726889619359a853a63e3f535aa6121a9
Paul Ablöscher <abloescherpaul@gmail.com>
2019-01-15 15:50:37 +0100

    Added AndComponent

---
+++ YALS/ComponentTest/Model/Connection/Connection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace ComponentTest.Model.Connection
+{
+    public class Connection : IConnection
+    {
+        public Connection(IPin output)
+        {
+            this.InputPins = new List<IPin>();
+            this.Output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        public IPin Output
+        {
+            get;
+            set;
+        }
+
+        public ICollection<IPin> InputPins
+        {
+            get;
+            set;
+        }
+
+        public void AddInputPin(IPin pin)
+        {
+            pin.Value = this.Output.Value;
+            this.InputPins.Add(pin);
+        }
+
+        public void RemoveInputPin(IPin pin)
+        {
+            if (this.InputPins.Remove(pin))
+            {
+                pin.Value = null;
+            }
+        }
+    }
+}
---
+++ YALS/ComponentTest/Model/Value/ValueGeneric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace ComponentTest.Model.Value
+{
+    public class ValueGeneric<T> : IValueGeneric<T>
+    {
+        public T Value
+        {
+            get;
+            set;
+        }
+        object IValue.Value
+        {
+            get
+            {
+                return this.Value;
+            }
+
+            set
+            {
+                this.Value = (T)value;
+            }
+        }
+    }
+}
