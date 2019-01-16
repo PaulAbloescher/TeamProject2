a5d05a3d80fd2541ac7d6119b5eba5907d2cf696
Christian Kohout <christian.kohout@gmx.at>
2019-01-16 12:06:22 +0100

    Added not component

---
+++ YALS/Components/Components/NotComponent.cs
@@ -0,0 +1,47 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Components
+{
+    public class NotComponent : Component
+    {
+        public NotComponent()
+        {
+            this.Setup();
+        }
+
+        public override void Execute()
+        {
+            var inputPin = this.Inputs.First();
+            var output = this.Outputs.First();
+
+            output.Value.Current = false;
+
+            if (inputPin.Value != null)
+            {
+                var firstValue = (bool)inputPin.Value.Current;
+                
+                output.Value.Current = !firstValue;
+            }
+        }
+
+        private void Setup()
+        {
+            this.Type = NodeType.Logic;
+            var inputPin1 = new Pin<bool>("Input1");
+            var outputPin1 = new Pin<bool>("Output1");
+            this.Inputs.Add(inputPin1);
+            this.Outputs.Add(outputPin1);
+            this.LoadImage();
+        }
+
+        private void LoadImage()
+        {
+            this.Picture = Properties.Resources.NotGate;
+        }
+    }
+}
