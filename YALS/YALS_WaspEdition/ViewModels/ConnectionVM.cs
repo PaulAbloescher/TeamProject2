06738fc3b8fbfa6309f06058dabedd0909d1761a
Christian Kohout <christian.kohout@gmx.at>
2019-01-23 15:34:38 +0100

    trying to fix conflict

--- YALS/YALS_WaspEdition/ViewModels/ConnectionVM.cs
+++ YALS/YALS_WaspEdition/ViewModels/ConnectionVM.cs
@@ -6,50 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Media;
-using YALS_WaspEdition.Commands;
-
-namespace YALS_WaspEdition.ViewModels
-{
-    [Serializable()]
-    public class ConnectionVM : INotifyPropertyChanged
-    {
-        public ConnectionVM(PinVM outputPin, PinVM inputPin, ICommand disconnectCommand)
-        {
-            this.OutputPin = outputPin ?? throw new ArgumentNullException(nameof(outputPin));
-            this.InputPin = inputPin ?? throw new ArgumentNullException(nameof(inputPin));
-            this.DisconnectCommand = disconnectCommand ?? throw new ArgumentNullException(nameof(disconnectCommand));
-        }
-
-        public PinVM OutputPin
-        {
-            get;
-        }
-
-        public PinVM InputPin
-        {
-            get;
-        }
-
-        public Color ConnectionColor
-        {
-            get
-            {
-                return this.OutputPin.PinColor;
-            }
-        }
-
-        public string ConnectionAsString
-        {
-            get
-            {
-                return $"Output: {this.OutputPin.Pin.Label} -> Input: {this.InputPin.Pin.Label}";
-            }
-        }
-
-        public ICommand DisconnectCommand
-        {
-            get;
-using YALS_WaspEdition.Commands;
 
 namespace YALS_WaspEdition.ViewModels
 {
@@ -87,7 +43,5 @@ namespace YALS_WaspEdition.ViewModels
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-    }
-}
     }
 }
