8d8648364cdd74cb719d9b51405b5cb82aa1ce30
Christian Kohout <christian.kohout@gmx.at>
2019-01-24 14:21:49 +0100

    Pin is now style cop conform

--- YALS/Components/Components/Pin.cs
+++ YALS/Components/Components/Pin.cs
@@ -1,30 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Shared;
+﻿// ---------------------------------------------------------------------
+// <copyright file="Pin.cs" company="FHWN.ac.at">
+// Copyright(c) FHWN. All rights reserved.
+// </copyright>
+// <summary>Represents a pin on a component.</summary>
+// <author>Killerwasps</author>
+// ---------------------------------------------------------------------
 
 namespace Components.Components
 {
-    [Serializable()]
+    using System;
+    using Shared;
+
+    /// <summary>
+    /// Represents a pin on a component.
+    /// </summary>
+    /// <typeparam name="T">The type of the pin.</typeparam>
+    /// <seealso cref="Shared.IPinGeneric{T}" />
+    [Serializable]
     public class Pin<T> : IPinGeneric<T>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pin{T}"/> class.
+        /// </summary>
+        /// <param name="label">The label of the pin.</param>
         public Pin(string label)
         {
             this.Label = label ?? throw new ArgumentNullException(nameof(label));
             this.Value = new ValueGeneric<T>();
         }
 
+        /// <summary>
+        /// Gets or sets the value of the pin.
+        /// </summary>
+        /// <value>
+        /// The value of the pin.
+        /// </value>
         public IValueGeneric<T> Value
         {
             get;
-
             set;
         }
 
-        public string Label { get; set; }
+        /// <summary>
+        /// Gets or sets the label of the pin.
+        /// </summary>
+        /// <value>
+        /// The label of the pin.
+        /// </value>
+        public string Label
+        {
+            get;
+            set;
+        }
 
+        /// <summary>
+        /// Gets or sets the generic value of the pin.
+        /// </summary>
+        /// <value>
+        /// The generic value of the pin.
+        /// </value>
         IValue IPin.Value
         {
             get
