6088c095c29bf3265875a8a10c91a461244a3be5
Paul Ablöscher <abloescherpaul@gmail.com>
2019-01-24 15:45:38 +0100

    Some stylecop stuff.

--- YALS/YALS_WaspEdition/Converters/BitmapConverter.cs
+++ YALS/YALS_WaspEdition/Converters/BitmapConverter.cs
@@ -1,24 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
-using System.Windows.Media.Imaging;
+﻿// <copyright file="BitmapConverter.cs" company="KW Softworks">
+//     Copyright (c) Paul-Noel Ablöscher. All rights reserved.
+// </copyright>
+// <summary>Converts a bitmap to bitmap image.</summary>
+// <author>Killerwasps</author>
 
 namespace YALS_WaspEdition.Converters
 {
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows.Data;
+    using System.Windows.Media.Imaging;
+
+    /// <summary>
+    /// Converts a bitmap to bitmap image.
+    /// </summary>
+    /// <seealso cref="System.Windows.Data.IValueConverter" />
     public class BitmapConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             using (MemoryStream ms = new MemoryStream())
             {
-
                 Bitmap bitmap = (Bitmap)value;
                 bitmap.Save(ms, ImageFormat.Png);
                 ms.Position = 0;
@@ -30,10 +45,21 @@ namespace YALS_WaspEdition.Converters
                 result.Freeze();
                 return result;
             }
-
-
         }
 
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// Thrown method is called.
+        /// </exception>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
