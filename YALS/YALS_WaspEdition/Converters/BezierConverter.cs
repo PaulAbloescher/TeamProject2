2bd7da0ab15cd2efedebffcd029df395cad02733
DoxyGen <doxy@gen.org>
2019-01-24 15:36:39 +0100

    Formatted BezierConverter.cs according to stylecop rules.

--- YALS/YALS_WaspEdition/Converters/BezierConverter.cs
+++ YALS/YALS_WaspEdition/Converters/BezierConverter.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright file="BezierConverter.cs" company="KW Softworks">
-//     Copyright (c) Paul-Noel Ablöscher. All rights reserved.
+//     Copyright (c) Strommer Kilian. All rights reserved.
 // </copyright>
 // <summary>Represents the bezier line converter.</summary>
 // <author>Killerwasps</author>
@@ -12,24 +12,54 @@ namespace YALS_WaspEdition.Converters
     using System.Windows.Data;
     using System.Windows.Media;
 
+    /// <summary>
+    /// Implements a MultiValueConverter that takes four input coordinates and constructs a horizontal bezier path between them.
+    /// </summary>
     public class BezierConverter : IMultiValueConverter
     {
+        /// <summary>
+        /// Converts a line into a smooth bezier path.
+        /// </summary>
+        /// <param name="values">Int array with the format: X1,Y1,X2,Y2.</param>
+        /// <param name="targetType">The target type to convert to.</param>
+        /// <param name="parameter">Converter parameters.</param>
+        /// <param name="culture">Local culture.</param>
+        /// <returns>New bezier geometry.</returns>
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             var coords = values.Select(System.Convert.ToInt32).ToArray();
 
-            const int controlOffset = 100;
+            // The horizontal offset of the control points.
+            const int ControlOffset = 100;
+
             // M X1,Y1 C X1+100,Y1 X2-100,Y2 X2,Y2
-            return Geometry.Parse($"M {coords[0]},{coords[1]} C {coords[0] + controlOffset},{coords[1]} {coords[2]- controlOffset},{coords[3]} {coords[2]},{coords[3]}");
+            return Geometry.Parse($"M {coords[0]},{coords[1]} C {coords[0] + ControlOffset},{coords[1]} {coords[2]- ControlOffset},{coords[3]} {coords[2]},{coords[3]}");
         }
 
+        /// <summary>
+        /// Converts a line into a smooth bezier path.
+        /// </summary>
+        /// <param name="x1">X Coordinate of the start point.</param>
+        /// <param name="y1">Y Coordinate of the start point.</param>
+        /// <param name="x2">X Coordinate of the end point.</param>
+        /// <param name="y2">Y Coordinate of the end point.</param>
+        /// <returns>New bezier geometry.</returns>
         public Geometry Convert(int x1, int y1, int x2, int y2)
         {
-            const int controlOffset = 100;
+            // The horizontal offset of the control points.
+            const int ControlOffset = 100;
 
-            return Geometry.Parse($"M {x1},{y1} C {x1 + controlOffset},{y1} {x2 - controlOffset},{y2} {x2},{y2}");
+            return Geometry.Parse($"M {x1},{y1} C {x1 + ControlOffset},{y1} {x2 - ControlOffset},{y2} {x2},{y2}");
         }
 
+        /// <summary>
+        /// Unused in this converter.
+        /// </summary>
+        /// <param name="value">The parameter is not used.</param>
+        /// <param name="targetTypes">The parameter is not used.</param>
+        /// <param name="parameter">The parameter is not used.</param>
+        /// <param name="culture">The parameter is not used.</param>
+        /// <returns>Nothing. This is unused.</returns>
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
