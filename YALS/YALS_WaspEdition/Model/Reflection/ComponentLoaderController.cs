6ae0650eb5b2e62c4bf344f7caa3afc2904b849b
ChristineH122 <104428@fhwn.ac.at>
2019-01-24 16:04:13 +0100

    Stylecop

--- YALS/YALS_WaspEdition/Model/Reflection/ComponentLoaderController.cs
+++ YALS/YALS_WaspEdition/Model/Reflection/ComponentLoaderController.cs
@@ -12,15 +12,31 @@ namespace YALS_WaspEdition.Model.Component.Reflection
     using System.IO;
     using Shared;
     using YALS_WaspEdition.Model.Reflection;
+
+    /// <summary>
+    /// Represents the <see cref="ComponentLoaderController"/> class.
+    /// </summary>
     public class ComponentLoaderController : IComponentLoaderController
     {
+        /// <summary>
+        /// The component loader of the class.
+        /// </summary>
         private readonly IComponentLoader loader;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComponentLoaderController"/> class.
+        /// </summary>
+        /// <param name="loader">The component loader for the class.</param>
         public ComponentLoaderController(IComponentLoader loader)
         {
             this.loader = loader ?? throw new ArgumentNullException(nameof(loader));
         }
 
+        /// <summary>
+        /// Loads the <see cref="IDisplayableNode"/> items from the specified path.
+        /// </summary>
+        /// <param name="path">The path from which the assembly gets loaded.</param>
+        /// <returns>A <see cref="IDisplayableNode"/> dictionary sorted by <see cref="NodeType"/>.</returns>
         public IDictionary<NodeType, ICollection<IDisplayableNode>> Load(string path)
         {
             this.CreatePluginsFolder();
@@ -28,6 +44,9 @@ namespace YALS_WaspEdition.Model.Component.Reflection
             return result;
         }
 
+        /// <summary>
+        /// Creates the plugins folder.
+        /// </summary>
         private void CreatePluginsFolder()
         {
             Directory.CreateDirectory("Plugins");
