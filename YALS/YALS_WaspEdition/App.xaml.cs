53a21f3ce77a7e981714787026f379fd3b89ecef
DoxyGen <doxy@gen.org>
2019-01-24 15:40:42 +0100

    Formatted App.xaml.cs according to stylecop rules

--- YALS/YALS_WaspEdition/App.xaml.cs
+++ YALS/YALS_WaspEdition/App.xaml.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
+﻿// ---------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="FHWN.ac.at">
+// Copyright(c) FHWN. All rights reserved.
+// </copyright>
+// <summary>The component for simulating a logical AND in a logic simulation.</summary>
+// <author>Killerwasps</author>
+// ---------------------------------------------------------------------
 namespace YALS_WaspEdition
 {
+    using System.Windows;
+
     /// <summary>
-    /// Interaktionslogik für "App.xaml"
+    /// Interaction logic for "App.xaml".
     /// </summary>
     public partial class App : Application
     {
